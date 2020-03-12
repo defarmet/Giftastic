28e9f9ebef6a9d48390385baa4d50ef0081c71fb
defarmet <defarmet@protonmail.com>
2020-03-12 13:22:51 -0400

    Added buttons for getting topics, adding buttons, and mobile scaling

--- assets/javascript/giphy.js
+++ assets/javascript/giphy.js
@@ -1,6 +1,6 @@
-var topic = "";
+var topics = ["bird", "walt disney", "epcot"];
 
-function capitalize() {
+function capitalize(topic) {
 	var str = topic.split(" ");
 	for (var i = 0; i < str.length; i++) {
 		str[i] = str[i][0].toUpperCase() + str[i].substr(1);
@@ -8,9 +8,9 @@ function capitalize() {
 	return str.join(" ");
 }
 
-function generate_card() {
+function generate_card(topic) {
 	var card = $("<div>").addClass("card");
-	card.append($("<h5>").addClass("card-header").text(capitalize()));
+	card.append($("<h5>").addClass("card-header").text(capitalize(topic)));
 	return card;
 }
 
@@ -26,14 +26,14 @@ function generate_gif(images) {
 function generate_div(data) {
 	var gif_div = $("<div>").addClass("gif");
 	gif_div.append(generate_gif(data.images));
-	gif_div.append($("<p>").text(data.rating));
+	gif_div.append($("<p>").text(data.rating.toUpperCase()));
 	return gif_div;
 }
 
 function display_gifs(response) {
 	var gif_row = $("<div>").addClass("card-body scroll");
 	$("#gifs").prepend($("<br>"));
-	$("#gifs").prepend(generate_card().append(gif_row));
+	$("#gifs").prepend(generate_card(topic).append(gif_row));
 	for (var i = 0; i < response.data.length; i++) {
 		gif_row.append(generate_div(response.data[i]));
 	}
@@ -49,14 +49,35 @@ function toggle_animation() {
 	}
 }
 
-function display_topic(e) {
-	e.preventDefault();
-	topic = $("#query").val().trim();
+function get_topic() {
+	topic = $(this).attr("value");
 	$.ajax({
 		url: "https://api.giphy.com/v1/gifs/search?api_key=Y489HiM8mmuPI3SU3kkniTTFUTGMj1L2&q=&limit=10&offset=0&lang=en&q=" + topic,
 		method: "GET"
-	}).then(display_gifs);
+	}).then(function(response) {
+		display_gifs(response, topic);
+	});
+}
+
+function add_button(topic) {
+	var button = $("<button>").addClass("btn get_gifs");
+	button.attr("value", topic);
+	button.text(capitalize(topic));
+	$("#header").append(button);
+}
+
+function add_topic(e) {
+	event.preventDefault();
+	topic = $("#query").val().trim();
+	$("#query").val("");
+	topics.push(topic);
+	add_button(topic);
+}
+
+for (var i = 0; i < topics.length; i++) {
+	add_button(topics[i]);
 }
 
 $(document).on("click", "img", toggle_animation);
-$(document).on("click", "#search", display_topic);
+$(document).on("click", "#search", add_topic);
+$(document).on("click", ".get_gifs", get_topic);
